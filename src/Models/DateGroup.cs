b74384766e4f9db9aea8d08084b7d308b6325a15
Enes Arican <Enes.Arican@accounttechnologies.com>
2020-05-28 19:34:52 +0100

    working datagrop model

--- src/Models/DateGroup.cs
+++ src/Models/DateGroup.cs
@@ -1,10 +1,28 @@
-﻿
+﻿using System;
+
 namespace ReportGenerator.Models
 {
-    public class DateGroup
+    public class DateGroup : IEquatable<DateGroup>
     {
         public int Month { get; set; }
 
         public int Year { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as DateGroup);
+        }
+
+        public bool Equals(DateGroup other)
+        {
+            return other != null &&
+                   Month == other.Month &&
+                   Year == other.Year;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Month, Year);
+        }
     }
 }
--- src/Services/DataWriterService.cs
+++ src/Services/DataWriterService.cs
@@ -14,76 +14,15 @@ namespace ReportGenerator.Services
     {
         public void WriteData(IList<PersonData> personDataList, Excel.Workbook workbook)
         {
-
-
             //var test = personDataList.SelectMany(p => p.AttendanceRecord.Select(a => new { name = p.FirstName, surname = p.LastName, date = a.Key, attedance = a.Value}));
 
-            var dateGroupsPerMonth = personDataList.SelectMany(p => p.AttendanceRecord.Keys).Distinct().GroupBy(g => new DateGroup{ Year = g.Year, Month = g.Month });
-
+            var dateGroupsPerMonth = personDataList.SelectMany(
+                p => p.AttendanceRecord.Keys).Distinct().GroupBy(
+                    g => new DateGroup { Year = g.Year, Month = g.Month });
 
             GenerateWorkbook(workbook, dateGroupsPerMonth, personDataList);
 
-            var sheet = (Excel.Worksheet)workbook.ActiveSheet;
-              
-            CultureInfo cultureInfoTR = new CultureInfo("tr-TR");
-            CultureInfo cultureInfoGB = new CultureInfo("en-GB");
-
-            int y = 0, x = 0, sheetCount = 1;
-            int headerRow = 3, dateRow = 2, atStrtCol = 4, atEndCol;
-
-            foreach (var monthDateGroup in dateGroupsPerMonth.OrderByDescending(d => new DateTime(d.Key.Year, d.Key.Month, 1))) 
-            {
-                sheet = (Excel.Worksheet)workbook.Sheets[sheetCount];
-                sheet.Activate();
-
-                sheet.Cells[headerRow, 1] = "Adi";
-                sheet.Cells[headerRow, 2] = "Soyadi";
-                sheet.Cells[headerRow, 3] = "Telefon";
-
-                y = headerRow + 1;
-                foreach (var personData in personDataList.OrderBy(p => p.FirstName))
-                {
-                    sheet.Cells[y, 1] = personData.FirstName;
-                    sheet.Cells[y, 2] = personData.LastName;
-                    sheet.Cells[y, 3] = personData.PhoneNumber;
-                    
-
-                    CultureInfo.CurrentCulture = cultureInfoTR;
-                    
-                    //atEndCol = atStrtCol + dateGroup.Count();
-                    //var strtCell = sheet.Cells[1, atStrtCol];
-                    //var endCell = sheet.Cells[1, atEndCol];
-                    //var monthHeader = sheet.Range[strtCell,endCell];
-                    //monthHeader.Merge();
-
-                    sheet.Cells[1,atStrtCol] = monthDateGroup.Select(d => d.ToString("MMMM")).First(); 
-
-
-                    x = atStrtCol;
-                    foreach (var date in monthDateGroup.OrderBy(d => d.Day))
-                    {
-                        sheet.Cells[headerRow, x] = date.ToString("dddd");
-                        sheet.Cells[dateRow, x] = date;
-
-                        if (personData.AttendanceRecord.TryGetValue(date, out var value)) 
-                            sheet.Cells[y, x] = value.ToString();
-                        x++;
-                    }
-                    y++;
-                    CultureInfo.CurrentCulture = cultureInfoGB;
-                }
-                sheetCount++;
-                workbook.Sheets.Add(After: sheet);
-
-                Excel.Range mobileColumn = (Excel.Range)sheet.Columns[3];
-                mobileColumn.NumberFormat = "#### ### #### ###";
-
-                Excel.Range dateRowRange = (Excel.Range)sheet.Rows[dateRow];
-                dateRowRange.NumberFormat = "dd-MM-yyyy";
-
-
-            }
-            sheet = (Excel.Worksheet)workbook.Sheets[1];
+            var sheet = (Excel.Worksheet)workbook.Sheets[1];
             sheet.Activate();
 
         }
@@ -103,10 +42,6 @@ namespace ReportGenerator.Services
             {
                 sheet = (Excel.Worksheet)workbook.Sheets[sheetCount];
                 sheet.Activate();
-
-                sheet.Cells[headerRow, 1] = "Adi";
-                sheet.Cells[headerRow, 2] = "Soyadi";
-                sheet.Cells[headerRow, 3] = "Telefon";
 
                 y = headerRow + 1;
                 foreach (var personData in personDataList.OrderBy(p => p.FirstName))
@@ -143,13 +78,25 @@ namespace ReportGenerator.Services
                 sheetCount++;
                 workbook.Sheets.Add(After: sheet);
 
-                Excel.Range mobileColumn = (Excel.Range)sheet.Columns[3];
-                mobileColumn.NumberFormat = "#### ### #### ###";
+                FormatWorksheet(sheet, headerRow, dateRow);
 
-                Excel.Range dateRowRange = (Excel.Range)sheet.Rows[dateRow];
-                dateRowRange.NumberFormat = "dd-MM-yyyy";
             }
 
+            //remove last sheet
+
+        }
+
+        private void FormatWorksheet(Excel.Worksheet sheet, int headerRow, int dateRow) 
+        {
+            sheet.Cells[headerRow, 1] = "Adi";
+            sheet.Cells[headerRow, 2] = "Soyadi";
+            sheet.Cells[headerRow, 3] = "Telefon";
+
+            Excel.Range mobileColumn = (Excel.Range)sheet.Columns[3];
+            mobileColumn.NumberFormat = "#### ### #### ###";
+
+            Excel.Range dateRowRange = (Excel.Range)sheet.Rows[dateRow];
+            dateRowRange.NumberFormat = "dd-MM-yyyy";
         }
     }
 }
